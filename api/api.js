7da74036a7b65449a825fe332eb9d97b183fab7d
Adi Mora <adi.mora@gamesys.co.uk>
2016-01-13 09:25:26 +0000

    api docs

--- api/api.js
+++ api/api.js
@@ -2,6 +2,7 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "Waud",
+        "WaudFocusManager",
         "WaudSound",
         "WaudSoundOptions",
         "WaudUtils"
