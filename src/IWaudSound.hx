598faaf1692ffcde81d2dc3574f9362421d36af2
Adi <adiinteractive@gmail.com>
2016-06-08 10:19:28 +0100

    added new methods (setTime, getTime) and duration property

--- src/IWaudSound.hx
+++ src/IWaudSound.hx
@@ -6,11 +6,14 @@
 interface IWaudSound {
 	var url:String;
 	var isSpriteSound:Bool;
+	var duration(get, null):Float;
 	function load(?callback:IWaudSound -> Void):IWaudSound;
 	function play(?spriteName:String, ?soundProps:AudioSpriteSoundProperties):Int;
 	function togglePlay():Void;
 	function stop():Void;
 	function pause():Void;
+	function setTime(time:Float):Void;
+	function getTime():Float;
 	function onEnd(callback:IWaudSound -> Void):IWaudSound;
 	function onLoad(callback:IWaudSound -> Void):IWaudSound;
 	function onError(callback:IWaudSound -> Void):IWaudSound;
