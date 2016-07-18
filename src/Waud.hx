7cdb0aeb87b4bf40b8a9f01658f8c8dfdd60360f
Adi <adiinteractive@gmail.com>
2016-07-18 13:56:34 +0100

    Update Waud.hx

--- src/Waud.hx
+++ src/Waud.hx
@@ -20,7 +20,7 @@ import js.Browser;
 	* @static
 	* @type {String}
 	*/
-	public static var version:String = "0.5.4";
+	public static var version:String = "0.6.1";
 
 	/**
 	* Tells whether to use web audio api or not.
