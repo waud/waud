e3bbc24404961926f32c2592e20e9d4e2a963e15
Adi <adiinteractive@gmail.com>
2016-07-26 16:08:31 +0100

    updated version

--- src/Waud.hx
+++ src/Waud.hx
@@ -20,7 +20,7 @@ import js.Browser;
 	* @static
 	* @type {String}
 	*/
-	public static var version:String = "0.6.4";
+	public static var version:String = "0.6.5";
 
 	/**
 	* Tells whether to use web audio api or not.
