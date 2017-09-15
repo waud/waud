b3866d3fa545c5cae73d20426fe6b7294762c659
Adi Mora <adireddy@users.noreply.github.com>
2017-09-15 16:59:09 +0100

    New release

--- src/Waud.hx
+++ src/Waud.hx
@@ -20,7 +20,7 @@ import js.Browser;
 	* @static
 	* @type {String}
 	*/
-	public static var version:String = "0.9.12";
+	public static var version:String = "0.9.13";
 
 	/**
 	* Tells whether to use web audio api or not.
